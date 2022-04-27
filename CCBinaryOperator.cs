4c3b68f7bdd16d337bbd33fba25680267a96de15
Riccardo Alni <93580685+AhVaBene@users.noreply.github.com>
2022-04-27 17:21:41 +0200

    Add files via upload

---
+++ CCBinaryOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP21_Calculator.Alni
+{
+    public class CCBinaryOperator
+    {
+        int Prec { get; }
+        Type Op_Type { get; }
+        Func<double, double, double> _op;
+
+        public CCBinaryOperator(Func<double, double, double> op, int prec, Type type)
+        {
+            Op_Type = type;
+            Prec = prec;
+            _op = op;
+        }
+        public double apply(double a, double b) => _op.Invoke(a, b);
+    }
+}
---
+++ Combinatoricsfactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace OOP21_Calculator.Alni
+{
+    public class Combinatoricsfactory
+    {
+        public static ICalculatorModel Create()
+        {
+            Dictionary<string, CCBinaryOperator> dic = new Dictionary<string, CCBinaryOperator>();
+            Dictionary<string, CCUnaryOperator> dic2 = new Dictionary<string, CCUnaryOperator>();
+            dic.Add("Somma", new CCBinaryOperator((x, y) => Sum(x, y), 0, null));
+            dic2.Add("Precedente", new CCUnaryOperator((x) => x - 1, 0, null));
+            return new CalculatorModelTemplate(dic, dic2);
+        }
+        static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
