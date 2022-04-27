f72175286c1163921c7c1c4f6ad838069b1e0320
Riccardo Alni <riccardo.alni@studio.unibo.it>
2022-04-27 17:36:34 +0200

    add some features

---
+++ Alni/CCUnaryOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP21_Calculator.Alni
+{
+    public class CCUnaryOperator
+    {
+        int Prec { get; }
+        Type Op_Type { get; }
+        readonly Func<double, double> _op;
+
+        public CCUnaryOperator(Func<double, double> op, int prec, Type type)
+        {
+            Op_Type = type;
+            Prec = prec;
+            _op = op;
+        }
+        public double apply(double a) => _op.Invoke(a);
+    }
+}
---
+++ Alni/CalculatorModelTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace OOP21_Calculator.Alni
+{
+    public class CalculatorModelTemplate : ICalculatorModel
+    {
+        private readonly Dictionary<string, CCBinaryOperator> _b_map;
+        private readonly Dictionary<string, CCUnaryOperator> _u_map;
+
+        public Dictionary<string, CCBinaryOperator> Get_Binary_Ops()
+        {
+            return _b_map;
+        }
+        public Dictionary<string, CCUnaryOperator> Get_Unary_Ops()
+        {
+            return _u_map;
+        }
+        public CalculatorModelTemplate(Dictionary<string, CCBinaryOperator> b_map, Dictionary<string, CCUnaryOperator> u_map)
+        {
+            _b_map = b_map;
+            _u_map = u_map;
+        }
+    }
+}
